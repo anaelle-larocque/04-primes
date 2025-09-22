29b70a6653b5f58997024e1df83de6ebb8d8e142
anaelle-larocque <anaelle.larocque@edu.esiee.fr>
2025-09-22 11:52:01 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from math import sqrt
 
-#### Fonction secondaire
-
-
 def isprime(p):
-
-    # votre code ici
-
-    pass
-
-#### Fonction principale
+    if p < 2:
+        return False
+    else :
+        for k in range(2, int(sqrt(p) + 1 )):
+            if p%k == 0:
+                return False
+        return True
 
 
 def main():
+    n = int(input("Entrez un entier: "))
+    if isprime(n):
+        print(f"{n} est un nombre premier.")
+    else:
+        print(f"{n} n'est pas un nombre premier.")
 
-    # vos appels à la fonction secondaire ici
 
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
-
 
 if __name__ == "__main__":
     main()
